720a6a6cef39314b38ee92801a823fbb7a4b9493
Ole Henrik Dahle <oda@indra.no>
2022-10-06 15:11:33 +0200

    Now also calculating average distance flown and average flying time.

--- analyse.pl
+++ analyse.pl
@@ -5,6 +5,41 @@ print("Analysing log files in: $directory\n");
 $landed = `grep landing ${directory}/USEPEFLIGHT* | wc -l`;
 print("Completed flights: $landed");
 
+# Calculate average distance flown
+@flightfiles = glob " ${directory}/USEPEFLIGHT*";
+
+if (@flightfiles > 1) {
+    # decide what to do here if there are no matching files,
+    # or multiple matching files
+    die("More than 1 LOS log in ${directory}!");
+} elsif (@flightfiles > 1) {
+    # decide what to do here if there are no matching files,
+    # or multiple matching files
+    die("No LOS log in ${directory}?");
+}
+open(FFLIGHT, '<', $flightfiles[0]) or die("Failed to open $losfile[0]");
+@flights;
+$totalDistance = 0.0;
+$totalTime = 0.0;
+while(<FFLIGHT>){
+    if($_ =~ /^(\d+.\d+),([\w\d_]+),takeoff,/) {
+	$flights{$2} = $1;
+#	print($2, " takeoff after ", $flights{$2}, " seconds\n");
+    }
+    if($_ =~ /(\d+.\d+),([\w\d_]+),landing,(\d+.\d+)$/) {
+	$flightTime = $1 - $flights{$2};
+#	print($2, " landed after ", $flightTime, " seconds, ", $3, " meters\n");
+	$totalTime += $flightTime;
+	$totalDistance += $3;
+    }
+}
+close(FFLIGHT);
+$avgerageDistance = $totalDistance / $landed;
+print("Average distance flown: $avgerageDistance\n");
+print("Totaltime: $totalTime\n");
+$averageTime = $totalTime / $landed;
+print("Average flight time: $averageTime\n");
+
 $conflicts = `grep ,start ${directory}/USEPECONF* | wc -l`;
 print("Conflicts started: $conflicts");
 
