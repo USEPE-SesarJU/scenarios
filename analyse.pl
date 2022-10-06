25e8d56bc734bf8ee50eaa5b44678e769c298e19
Ole Henrik Dahle <oda@indra.no>
2022-10-06 13:51:30 +0200

    Now also counting the number of NMACs correctly.

--- analyse.pl
+++ analyse.pl
@@ -15,4 +15,29 @@ $los = `grep start ${directory}/USEPELOS* | wc -l`;
 print("LOS events: $los");
 
 
+# Count number of NMAC events: CPA < 3.75 m
+@cpafiles = glob " ${directory}/USEPECPA*";
 
+if (@cpafiles > 1) {
+    # decide what to do here if there are no matching files,
+    # or multiple matching files
+    die("More than 1 LOS log in ${directory}!");
+} elsif (@cpafiles > 1) {
+    # decide what to do here if there are no matching files,
+    # or multiple matching files
+    die("No LOS log in ${directory}?");
+}
+open(FCPA, '<', $cpafiles[0]) or die("Failed to open $losfile[0]");
+$nmacs = 0;
+while(<FCPA>){
+#    print $_;
+    if($_ =~ /,(\d+.\d+)$/) {
+	#print("CPA is ", $1, "\n");
+	if ($1 < 3.75) {
+	    $nmacs++;
+	}
+    }
+}
+print("NMAC events: $nmacs");
+close(FCPA);
+print("\n");
